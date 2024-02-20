9c42e0bc1942e8f2b997d77bac1dde9ef00769d8
deboraj-roy <deborajroy123@gmail.com>
2024-02-20 14:52:04 +0600

    API run with Person

---
+++ ASP_BasicAPI/Controllers/DebAPIController.cs
@@ -0,0 +1,20 @@
+﻿using ASP_BasicAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_BasicAPI.Controllers
+{
+    [Route("api/DebAPI")]
+    [ApiController]
+    public class DebAPIController : Controller 
+    {
+        [HttpGet]
+        public IEnumerable<Person> Persons()
+        {
+            return new List<Person>
+            {
+                new Person{ Id = 1, Name = "Sujon Roy"},
+                new Person{ Id = 2, Name = "Polok Roy"}
+            };
+        }
+    }
+}
